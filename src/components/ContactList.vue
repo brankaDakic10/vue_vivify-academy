8ab595e1f138db69f36ff3f6311ae1cb7493d43c
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-26 22:10:27 +0200

    Added ContactList

---
+++ src/components/ContactList.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+<h1>Contact List</h1>
+<ul class="list-group">
+    <li class="list-group-item"
+     v-for="(contact ,key) in contacts" 
+     :key="key">{{contact.name}}</li>
+
+
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+ props:['contacts']
+}
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,10 @@
 <template>
- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <router-link class="navbar-brand" to="contacts">My Contacts</router-link>
-    
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <router-link class="nav-item nav-link" to="contacts">Contacts</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-item nav-link" to="add-contact">Add Contact</router-link>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<router-link class="navbar-brand" to="/">My Contacts</router-link>
+<div class="navbar-nav">
+<router-link to="contacts" class="nav-link">Contacts</router-link>
+<router-link to="add-contact" class="nav-link">Add Contact</router-link>
+</div>
 </nav>
 </template>
 <script>
