57234da2be193370b933cdbb92823b7b137d5d61
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-26 21:37:29 +0200

    Added new

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <button class="btn btn-primary">Hello Bootstrap</button>
-   
-   
+ 
+   <NavBar/>
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
